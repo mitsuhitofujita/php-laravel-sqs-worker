8d0e4aab5a3ea75f9e4068a2ff6ad27e4608d5a3
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2024-01-14 04:58:25 +0000

    Add SQS worker command

---
+++ laravel-app/app/Console/Commands/Sqs/Worker.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Sqs;
+
+use Illuminate\Console\Command;
+
+class Worker extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sqs:worker';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
